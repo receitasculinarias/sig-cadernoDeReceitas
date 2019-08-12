d3380c50b630dd794ccea26ca550613cdfd4db5f
Anniellyfs <annyfs2015@gmail.com>
2019-08-12 15:47:03 -0300

    sobre

--- mylib.c
+++ mylib.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void menuPrincipal( void ){
     system ("clear");
     printf("===== SIG-Recipes: CADERNO DE RECEITAS CULINÁRIAS =====\n\n");
@@ -9,4 +8,10 @@ void menuPrincipal( void ){
     printf("2 - modificar receita\n");
     printf("3 - consultar receita\n");
     printf("4 - deletar receita\n");
+    printf("5 - sobre\n");
+}
+
+void sobre( void ){
+    printf("\n\nPrograma desenvolvido por:\n")
+    printf("Annielly Ferreira de Sousa e Rayra Kelly Araújo dos Santos")
 }

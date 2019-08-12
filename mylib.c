27feec8d41f2e1ea7e47674616c39741e400bacc
Anniellyfs <annyfs2015@gmail.com>
2019-08-12 14:34:16 -0300

    criação do menu principal

---
+++ mylib.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void menuPrincipal( void ){
+    system ("clear");
+    printf("===== SIG-Recipes: CADERNO DE RECEITAS CULINÁRIAS =====\n\n");
+    printf("1 - cadastrar receita\n");
+    printf("2 - modificar receita\n");
+    printf("3 - consultar receita\n");
+    printf("4 - deletar receita\n");
+}
